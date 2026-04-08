5dc78f703de5ee4fb4d6f8b1655a526cbe6f12d7
imen-mekni <113175332+imen-mekni@users.noreply.github.com>
2026-04-08 15:02:30 +0100

    Update index.js

--- backEnd/Mysql/index.js
+++ backEnd/Mysql/index.js
@@ -1,16 +1,9 @@
 const { Sequelize, DataTypes } = require("sequelize");
 require("dotenv").config();
 
-const connection = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-  }
-);
-
+const connection = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "mysql",
+});
 // verify connection
 connection.authenticate()
   .then(() => console.log("connection established successfully"))
