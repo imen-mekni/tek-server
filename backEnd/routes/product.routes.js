ff75ad7846f12e8c3d45c1573a6f16929e3d41ef
imen-mekni <113175332+imen-mekni@users.noreply.github.com>
2026-04-10 20:07:42 +0100

    Update product.routes.js

--- backEnd/routes/product.routes.js
+++ backEnd/routes/product.routes.js
@@ -3,7 +3,7 @@ const Router = express.Router();
 const { getAll,addProduct,updateProduct ,DeleteProduct , getOneProduct} = require("../controllers/product.controller");
 Router.get("/", getAll);
 Router.post("/add",addProduct)
-Router.patch("/:id",updateProduct)
+Router.put("/:id",updateProduct)
 Router.delete("/:id",DeleteProduct)
 Router.get("/:id", getOneProduct);
 module.exports = Router;
