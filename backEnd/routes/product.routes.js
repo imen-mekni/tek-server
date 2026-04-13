17280bff7955bba01292a3734db4bbed0af3f98a
imen-mekni <113175332+imen-mekni@users.noreply.github.com>
2026-04-13 13:51:36 +0100

    Update product.routes.js

--- backEnd/routes/product.routes.js
+++ backEnd/routes/product.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const Router = express.Router();
-const { getAll,addProduct,updateProduct ,DeleteProduct , getOneProduct} = require("../controllers/product.controller");
+const { getAll,addProduct,updateProduct ,DeleteProduct , getOneProduct , addToCart } = require("../controllers/product.controller");
 Router.get("/", getAll);
 Router.post("/add",addProduct)
 Router.put("/:id",updateProduct)
 Router.delete("/:id",DeleteProduct)
 Router.get("/:id", getOneProduct);
+Router.post("/cart/:cartId" , addToCart)
 module.exports = Router;
