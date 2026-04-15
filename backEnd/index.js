b987f36c8e50c39c01fa1d1b69be50374e13b277
imen-mekni <113175332+imen-mekni@users.noreply.github.com>
2026-04-15 18:20:04 +0100

    Update index.js

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const productRoute = require("./routes/product.routes");
-//uncomment to use mongodb
-// const db = require('./mongoDb')
-// uncomment to use MYSQL
+const cartroute = require("./routes/cartroutes");
+
 const db = require("./Mysql");
 
 const port = 5000;
@@ -14,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/products", productRoute);
+app.use("/api/cart",cartroute );
 
 app.listen(port, () => {
   console.log(`listening on ${port}`);
